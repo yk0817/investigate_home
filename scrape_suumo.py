201e8829b3bf56116ea4f9771b22686f5527c36c
山本憲 <em072029@gmail.com>
2017-06-09 16:21:41 +0900

    結合の仕方変更

--- scrape_suumo.py
+++ scrape_suumo.py
@@ -11,20 +11,41 @@ soup = BeautifulSoup(html, "lxml")
 
 scaped_wrappers = soup.find_all("div", class_="cassetteitem")
 
+# 位置引数から辞書を返す
+# 配列→辞書型にしたい。
+def minutes_dicts(key,text):
+    dict = {}
+    dict[key] = text
+    return dict
+
+
+
+
+
+
 for scrape in scaped_wrappers:
     dict = {}
-    address = scrape.select(".cassetteitem_detail-col1")
-
+    address = scrape.find(class_="cassetteitem_detail-col1").string
+    title_content = scrape.find(class_="cassetteitem_content-title").string
     # 東急田園都市線/三軒茶屋駅 歩9分
     # 東急世田谷線/西太子堂駅 歩11分
     # 東急世田谷線/若林駅 歩15分←といった情報のため、配列化
-    minutes_from_station = scrape.find_all(class_="cassetteitem_detail-text")
+    minutes_stations = scrape.find_all(class_="cassetteitem_detail-text")
+    count = 1
+    # 普通に入れていく。
+    for minute in minutes_stations:
+        base_key = "minute_station"
+        enter_key = base_key + str(count)
+        # 結合していく
+        dict.update(minutes_dicts(enter_key,minute.string))
+        count += 1
+    
+        
     # 配列後置
     # 築〜年という数字、〜階建て
     # 正規表現でメモリー化させる。
     scrape.find_all(class_="cassetteitem_detail-col3")
     # 家賃農法を配列化
     scrape.select(".cassetteitem_other > tbody")
-    sys.exit()
     
     
