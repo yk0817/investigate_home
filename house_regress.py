fc387cc5cc931e105e4561efce439433e7a4dba0
山本憲 <em072029@gmail.com>
2017-07-05 22:37:06 +0900

    内包表記リスト化

--- house_regress.py
+++ house_regress.py
@@ -1,13 +1,11 @@
 from common import *
 
 with connection.cursor() as cursor:
-    sql = "select room_rent from house_price;"
+    sql = "select room_rent,room_area from house_price;"
     cursor.execute(sql)
     results = cursor.fetchall()
     # 内包表記で計算してみる
-    # print(results)
-    # for row in results:
-    #     print(row)
-    train_x = [row.values() for row in results]
-    test = np.asarray(train_x)
-    print(test)
+    train_y = [np.asarray(row['room_rent']) for row in results]
+    train_x = [np.asarray(row['room_area']) for row in results]
+    # test = np.asarray(train_x)
+    # print(test)
