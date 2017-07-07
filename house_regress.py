0f410295209606bc864cc93ae2d0a81f3bbe9287
山本憲 <em072029@gmail.com>
2017-07-07 23:50:29 +0900

    配列化が疑問点

--- house_regress.py
+++ house_regress.py
@@ -1,11 +1,14 @@
 
 # coding: utf-8
 
-# In[18]:
+# In[8]:
 
 from common import *
 rng = np.random
 
+learning_rate = 0.01
+training_epochs = 1000
+display_step = 50
 
 with connection.cursor() as cursor:
     sql = "select room_rent,room_area from house_price;"
@@ -13,7 +16,7 @@ with connection.cursor() as cursor:
     results = cursor.fetchall()
     # 内包表記で計算してみる
     train_Y = [np.asarray(row['room_rent']) for row in results]
-    train_X = [np.asarray(row['room_area']) for row in results]
+    train_X = [int(np.asarray(row['room_area'])) for row in results]
     n_samples = train_X
     X = tf.placeholder("float")
     Y = tf.placeholder("float")
@@ -23,7 +26,69 @@ with connection.cursor() as cursor:
     b = tf.Variable(rng.randn(), name="bias")
     pred = tf.add(tf.multiply(X, W), b)
     cost = tf.reduce_sum(tf.pow(pred-Y, 2))/(2*n_samples)
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
     
+    init = tf.global_variables_initializer()
+    with tf.Session() as sess:
+        sess.run(init)
+        for epoch in range(training_epochs):
+            print(optimizer)
+            for (x, y) in zip(train_X, train_Y):
+                sess.run(optimizer, feed_dict={X: x, Y: y})
+                print(test)
+            # Display logs per epoch step
+            if (epoch+1) % display_step == 0:
+                c = sess.run(cost, feed_dict={X: train_X, Y:train_Y})
+        print("Optimization Finished!")
+        training_cost = sess.run(cost, feed_dict={X: train_X, Y: train_Y})
+        print("Training cost=", training_cost, "W=", sess.run(W), "b=", sess.run(b), '\n')
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+from common import *
+rng = np.random
+
+learning_rate = 0.01
+training_epochs = 1000
+display_step = 50
+
+with connection.cursor() as cursor:
+    sql = "select room_rent,room_area from house_price;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    # 内包表記で計算してみる
+    train_Y = [np.asarray(row['room_rent']) for row in results]
+    train_X = [int(np.asarray(row['room_area'])) for row in results]
+    n_samples = train_X
+    X = tf.placeholder("float")
+    Y = tf.placeholder("float")
+    # Set model weights
+    # 変数を定義    
+    W = tf.Variable(rng.randn(), name="weight")
+    b = tf.Variable(rng.randn(), name="bias")
+    pred = tf.add(tf.multiply(X, W), b)
+    cost = tf.reduce_sum(tf.pow(pred-Y, 2))/(2*n_samples)
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+    
+    init = tf.global_variables_initializer()
+    with tf.Session() as sess:
+        sess.run(init)
+        for epoch in range(training_epochs):
+            print(optimizer)
+            for (x, y) in zip(train_X, train_Y):
+                sess.run(optimizer, feed_dict={X: x, Y: y})
+            # Display logs per epoch step
+            if (epoch+1) % display_step == 0:
+                c = sess.run(cost, feed_dict={X: train_X, Y:train_Y})
+        print("Optimization Finished!")
+        training_cost = sess.run(cost, feed_dict={X: train_X, Y: train_Y})
+        print("Training cost=", training_cost, "W=", sess.run(W), "b=", sess.run(b), '\n')
 
 
 # In[ ]:
